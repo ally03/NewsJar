19a43ed3614fee25dc61fef5660c5d7eea0d3c81
Alid Didarali <aliddidarali@Alids-MacBook-Pro.local>
2019-06-10 19:24:07 +0530

    Scroll bar

--- src/components/applogo.js
+++ src/components/applogo.js
@@ -1,27 +1,41 @@
 import React from "react";
 import logo from "../logo-img.png";
 import "../App.css";
+// import * as Scroll from "react-scroll";
+import { animateScroll as scroll } from "react-scroll";
 
-function Applogo() {
-  return (
-    <header className="App-header">
-      <div>
-        <img src={logo} className="App-logo" alt="logo" />
-      </div>
-      <div>
-        <h1 className="font-logo">NewsJar</h1>
-      </div>
-      <div>
-        <button className="Header-button">News</button>
-      </div>
-      <div>
-        <button className="Header-button">Article</button>
-      </div>
-      <div>
-        <button className="Header-button">Readlist</button>
-      </div>
-    </header>
-  );
+class Applogo extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  scrollTo() {
+    scroll.scrollTo(750);
+  }
+
+  render() {
+    return (
+      <header className="App-header">
+        <div>
+          <img src={logo} className="App-logo" alt="logo" />
+        </div>
+        <div>
+          <h1 className="font-logo">NewsJar</h1>
+        </div>
+        <div>
+          <button className="Header-button" onClick={this.scrollTo}>
+            News
+          </button>
+        </div>
+        <div>
+          <button className="Header-button">Article</button>
+        </div>
+        <div>
+          <button className="Header-button">Readlist</button>
+        </div>
+      </header>
+    );
+  }
 }
 
 export default Applogo;
